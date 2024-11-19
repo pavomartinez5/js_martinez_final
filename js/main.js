55dab74e025d8fc5fa5d5167b4675e8406dc5b3a
Pavel Martinez <pavomartinez@hotmail.com>
2024-11-19 00:25:20 -0800

    Left off on problem 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 // Pavel Martinez
-console.log("Test");
-console.log("Test 2");
+//Final Project
+
+//1.createElemWithText
+
+const createElemWithText = (htmlElement = "p", textContent = "", className) => {
+  let newElement = document.createElement(htmlElement);
+  newElement.textContent = textContent;
+  if (className) {
+    newElement.className = className;
+  }
+  return newElement;
+};
+
+//2. createSelectOptions
+
+const createSelectOptions = (users) => {
+  if (!users) {
+    return undefined;
+  }
+
+  const options = [];
+
+  users.forEach((user) => {
+    const option = document.createElement("option");
+    option.value = user.id;
+    option.textContent = user.name;
+    options.push(option);
+  });
+
+  return options;
+};
